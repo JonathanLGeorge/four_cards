24ef40f9fb3eb2291e41c2dde1e7d702d54b99f2
Jonathan <jonathan.george.dev@gmail.com>
2020-09-08 03:35:03 -0700

    Updates

---
+++ precache-manifest.1f8c1142b843d91e724186e2ad99490a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbeec11c70289bfb8e0455d27b20d21",
+    "url": "/four_cards/index.html"
+  },
+  {
+    "revision": "0ecafdbde47ebafe6cac",
+    "url": "/four_cards/static/css/main.584442a2.chunk.css"
+  },
+  {
+    "revision": "732737c6cf4fca043aef",
+    "url": "/four_cards/static/js/2.71602eb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/four_cards/static/js/2.71602eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecafdbde47ebafe6cac",
+    "url": "/four_cards/static/js/main.bba8d030.chunk.js"
+  },
+  {
+    "revision": "d258b2c955169cdcf8a9",
+    "url": "/four_cards/static/js/runtime-main.4e908500.js"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/four_cards/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/four_cards/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/four_cards/static/media/icon-supervisor.8f7824b3.svg"
+  },
+  {
+    "revision": "8a1a6a173b407f9e45ba99fb7f25f0db",
+    "url": "/four_cards/static/media/icon-team-builder.8a1a6a17.svg"
+  }
+]);
